cc078bfe551d37329b12fddcb5bc88a03cf5b885
mknajeeb <mknajeeb@hotmail.com>
2026-03-09 00:32:14 -0400

    replaced api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,8 +1,8 @@
-// production api
-import axios from "axios";import axios from "axios";
+import axios from "axios";
 
 const API_BASE =
-  "https://laundryops-api-dsccuxa8c6dbghd9.centralus-01.azurewebsites.net";
+  "https://laundryops-api-dscucxa8c6dbghd9.centralus-01.azurewebsites.net";
 
 export const getOrders = () => axios.get(`${API_BASE}/orders`);
+
 export const getDashboard = () => axios.get(`${API_BASE}/dashboard`);
