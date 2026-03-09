bb209c217ba569300b8323206a46f4ae12872bcd
mknajeeb <mknajeeb@hotmail.com>
2026-03-09 01:03:38 -0400

    css changes and orderspage

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,8 +1,29 @@
 import axios from "axios";
 
 const API_BASE =
-  "https://laundryops-api-dscucxa8c6dbghd9.centralus-01.azurewebsites.net";
+"https://laundryops-api-dscucxa8c6dbghd9.centralus-01.azurewebsites.net";
 
-export const getOrders = () => axios.get(`${API_BASE}/orders`);
+/* ======================
+   DASHBOARD
+====================== */
 
-export const getDashboard = () => axios.get(`${API_BASE}/dashboard`);
+export const getDashboard = () =>
+  axios.get(`${API_BASE}/dashboard`);
+
+/* ======================
+   ORDERS
+====================== */
+
+export const getOrders = () =>
+  axios.get(`${API_BASE}/orders`);
+
+/* ======================
+   UPLOAD ORDERS
+====================== */
+
+export const uploadOrders = (formData) =>
+  axios.post(`${API_BASE}/upload_orders`, formData, {
+    headers: {
+      "Content-Type": "multipart/form-data"
+    }
+  });
